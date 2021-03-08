317c0ea637d6b002961828f34ce3e515d0ad8fa5
Antonio Raffaele Iannaccone <antonio.iannaccone7@gmail.com>
2021-03-08 20:29:08 +0100

    Added index.html + new job

--- freelancer_job_site/jobs/views.py
+++ freelancer_job_site/jobs/views.py
@@ -5,6 +5,8 @@ from django.db.models import Q # Needed for complex DB queries (adds and/or func
 
 def index(request):
     #jobs = Job.objects.all() # Select all from DB models
-    jobs = Job.objects.get(job_title__contains="Go") # Select from DB where job_title LIKE
-    jobs = Job.objects.values_list('job_title', 'job_description').filter(Q(job_title__contains="Py") | Q(job_title__contains="Go")) # Select specific columns from DB WHERE job_title LIKE
-    return HttpResponse(jobs)
+    #jobs = Job.objects.get(job_title__contains="Go") # Select from DB where job_title LIKE
+    #jobs = Job.objects.values_list('job_title', 'job_description').filter(Q(job_title__contains="Py") | Q(job_title__contains="Go")) # Select specific columns from DB WHERE job_title LIKE
+    jobs = Job.objects.values_list()
+    print(jobs)
+    return render(request, 'index.html', {'jobs':jobs})
